7014550864b87d9ad504b0d3c627260cbc309434
Little Coder <kimriana@yahoo.com>
2025-07-25 22:41:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -405,4 +405,5 @@ def asesmen_index():
 
 # ================== RUN APP ==================
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Railway otomatis mengisi PORT
+    app.run(host='0.0.0.0', port=port, debug=True)
